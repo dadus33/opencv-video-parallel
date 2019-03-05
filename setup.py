8aed02e42d8225f9357c6768f8689c448e86c5b1
Petrutiu Mihai <mihai.petrutiu@qubiz.com>
2019-03-05 13:58:27 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VideoParallelProcessing',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/MihaiTheCoder/opencv-video-parallel',
+    license='',
+    author='Mihai.Petrutiu',
+    author_email='',
+    description=''
+)
