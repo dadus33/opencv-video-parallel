dd2820bdf6370f4f208874590e8fdbad2d3a69f8
Petrutiu Mihai <mihai.petrutiu@qubiz.com>
2019-03-06 15:49:31 +0200

    update interval so that a dictionary can be passed as parameter

--- MultiProcessVideoPoolExecutor.py
+++ MultiProcessVideoPoolExecutor.py
@@ -20,29 +20,34 @@ class MultiProcessVideoPoolExecutor:
         else:
             self.number_of_processes = number_of_processes
 
-    def _process(self, interval):
+    def _process(self, interval_start, interval_stop, process_index, metadata):
+        cap = None
         try:
-            process_m = self.processing_factory(interval[2])
             cap = cv2.VideoCapture(self.video_path)
-            cap.set(cv2.CAP_PROP_POS_FRAMES, interval[0])
+            process_m = self.processing_factory(process_index, metadata)
+
+            cap.set(cv2.CAP_PROP_POS_FRAMES, interval_start)
             temp_list = []
-            print(f"start processing interval: {interval}")
-            current_frame = interval[0]
-            while current_frame <= interval[1]:
+            print(f"start processing interval: {(interval_start, interval_stop)}")
+            current_frame = interval_start
+            while current_frame <= interval_stop:
                 success, frame = cap.read()
                 result = process_m.process(frame, current_frame)
                 temp_list.append(result)
                 current_frame += 1
-            print(f"end processing interval: {interval}")
+            print(f"end processing interval: {(interval_start, interval_stop)}")
             return temp_list
         except Exception:
             traceback.print_exc()
             exit(1)
+        finally:
+            if cap is not None:
+                cap.release()
 
-    def start_processes(self):
+    def start_processes(self, metadata):
         pool = Pool(self.number_of_processes)
         self.intervals.sort(key=lambda x: x[0])
-        self.intervals = [(interval[0], interval[1], idx) for idx, interval in enumerate(self.intervals)]
+        self.intervals = [(interval[0], interval[1], idx, metadata) for idx, interval in enumerate(self.intervals)]
         self.intervals.sort(key=lambda x: x[1] - x[0])
         results = pool.map(self._process, self.intervals)
         return list(itertools.chain(*results))
